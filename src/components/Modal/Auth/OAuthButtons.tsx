18ec070979f7baca49104d63c9cddc3d6d7612b3
Stephen Mandoki <smandoki@outlook.com>
2023-07-18 17:11:08 +0800

    Completed sign in with Google
    users can now sign it using OAuth with Google. relevant error messages
    are displayed and loading spinner shows during sign in

--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -1,16 +1,48 @@
+import { FIREBASE_ERRORS } from "../../../firebase/errors";
+import { auth } from "../../../firebase/firebaseConfig";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+
 type Props = {};
 
 function OAuthButtons({}: Props) {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
   return (
     <div className="flex flex-col w-full my-4 gap-5">
-      <button className="flex text-gray-600 bg-white w-full rounded-full justify-center items-center gap-2 p-2 border border-gray-400 hover:bg-gray-50 active:bg-gray-100">
-        <img
-          src="/images/googlelogo.png"
-          alt="google logo"
-          className="h-5 w-5"
-        />
-        <p className="font-bold">Continue with Google</p>
-      </button>
+      {!loading && (
+        <button
+          onClick={() => signInWithGoogle()}
+          className="flex text-gray-600 bg-white w-full rounded-full justify-center items-center gap-2 p-2 border border-gray-400 hover:bg-gray-50 active:bg-gray-100"
+        >
+          <img
+            src="/images/googlelogo.png"
+            alt="google logo"
+            className="h-5 w-5"
+          />
+          <p className="font-bold">Continue with Google</p>
+        </button>
+      )}
+
+      {loading && (
+        <button
+          disabled
+          className="opacity-60 relative flex text-gray-600 bg-white w-full rounded-full justify-center items-center gap-2 p-2 border border-gray-400"
+        >
+          <img
+            src="/images/googlelogo.png"
+            alt="google logo"
+            className="h-5 w-5"
+          />
+          <p className="font-bold">Continue with Google</p>
+          <div className="absolute animate-spin border-4 rounded-full h-5 w-5 border-gray-300 border-t-gray-900"></div>
+        </button>
+      )}
+
+      {error && (
+        <p className="text-center text-red-500 -mt-4 text-sm">
+          {FIREBASE_ERRORS[error.message]}
+        </p>
+      )}
     </div>
   );
 }
--- src/firebase/errors.ts
+++ src/firebase/errors.ts
@@ -5,4 +5,6 @@ interface ErrorMap {
 export const FIREBASE_ERRORS: ErrorMap = {
   "Firebase: Error (auth/email-already-in-use).":
     "A user with that email already exists.",
+  "Firebase: Error (auth/popup-closed-by-user).":
+    "Popup was closed before sign in could complete.",
 };
